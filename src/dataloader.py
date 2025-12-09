3651ad29f70d6e7fab2e7b4dc93a66b42682a4e7
ganatra-v <ganatra.vaib@gmail.com>
2025-12-08 21:37:31 -0500

    [update] image size for vit compatibility

--- src/dataloader.py
+++ src/dataloader.py
@@ -214,7 +214,7 @@ class ComparisonDataset(Dataset):
 def load_image(img_path):
     image = cv2.imread(img_path)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    image = cv2.resize(image, (256, 256))
+    image = cv2.resize(image, (224, 224))
     return image
 
 
